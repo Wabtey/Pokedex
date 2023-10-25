5eb5b17fa1e34e38fcb847c5bb2e839a75726a30
Wabtey <wabtey@disroot.org>
2023-10-25 11:51:55 +0200

    rename to Pokedex

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'pokdemo'`, () => {
+  it(`should have as title 'pokedemo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('pokdemo');
+    expect(app.title).toEqual('pokedemo');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('pokdemo app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('pokedemo app is running!');
   });
 });
