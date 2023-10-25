db914a7886e8fd1e88a74233c99eaca58b91647a
Wabtey <wabtey@disroot.org>
2023-10-25 12:51:05 +0200

    Q6 - display all the map of pokemon with *ngFor

--- src/app/my-component/my-component.component.ts
+++ src/app/my-component/my-component.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PokemonType } from '../pokemon';
 
 @Component({
   selector: 'app-my-component',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class MyComponentComponent {
     id: string = '';
+
+    /**
+     * the key is their id -> value:
+     * [
+     *   name,
+     *   type,
+     *   weaknesses,
+     *   known
+     * ]
+     */
+    pokemonMap : Record<number, [string, PokemonType, PokemonType[], boolean]> = {
+        50 : ['Diglett', PokemonType.Ground, [PokemonType.Water, PokemonType.Grass, PokemonType.Ice], false],
+        51 : ['Dugtrio', PokemonType.Ground, [PokemonType.Water, PokemonType.Grass, PokemonType.Ice], false],
+        52 : ['Meowth', PokemonType.Normal, [PokemonType.Fighting], false],
+        53 : ['Persian', PokemonType.Normal, [PokemonType.Fighting], false],
+        54 : ['Psyduck', PokemonType.Water, [PokemonType.Grass, PokemonType.Electric], false],
+        55 : ['Goldduck', PokemonType.Water, [PokemonType.Grass, PokemonType.Electric], false]
+    }
 }
--- src/app/pokemon.ts
+++ src/app/pokemon.ts
@@ -1,5 +1,5 @@
 // from [pokedex](https://www.pokemon.com/us/pokedex/)
-enum PokemonType {
+export enum PokemonType {
     Water,
     Fire,
     Ice,
@@ -14,22 +14,4 @@ export class Pokemon {
     // type PokeDex = { 
     //     [name: string]: [string, boolean]; 
     // }
-
-    /**
-     * the key is their id -> value:
-     * [
-     *   name,
-     *   type,
-     *   weaknesses,
-     *   known
-     * ]
-     */
-    pokemonMap : Record<number, [string, PokemonType, PokemonType[], boolean]> = {
-        50 : ['Diglett', PokemonType.Ground, [PokemonType.Water, PokemonType.Grass, PokemonType.Ice], false],
-        51 : ['Dugtrio', PokemonType.Ground, [PokemonType.Water, PokemonType.Grass, PokemonType.Ice], false],
-        52 : ['Meowth', PokemonType.Normal, [PokemonType.Fighting], false],
-        53 : ['Persian', PokemonType.Normal, [PokemonType.Fighting], false],
-        54 : ['Psyduck', PokemonType.Water, [PokemonType.Grass, PokemonType.Electric], false],
-        55 : ['Goldduck', PokemonType.Water, [PokemonType.Grass, PokemonType.Electric], false]
-    }
 }
